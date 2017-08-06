31242fc477fde2a2756a86800b67f7874ef21c81
Rahmat H <kickyou44@gmail.com>
2017-08-06 18:58:23 +0700

    udahh

--- routes/addresses.js
+++ routes/addresses.js
@@ -4,27 +4,56 @@ const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('./db/data.db')
 
 router.get('/', (req, res) => {
-  db.all(`SELECT * FROM addresses`, (err, rows) => {
-    if (err) throw err
-    res.render('addresses', {data: rows})
+  db.all(`
+    SELECT
+      addresses.*,
+      contacts.name as contact_name
+
+      FROM addresses
+        LEFT JOIN contacts 
+          ON contacts.id=addresses.contact_id
+  `, (errA, rowsA) => {
+    if (errA) throw errA
+    db.all(`SELECT id, name FROM contacts`, (errC, rowsC) => {
+      res.render('addresses', {
+        data: rowsA,
+        contacts: rowsC
+      })
+    })
   })
 })
 
 router.post('/', (req, res) => {
-  db.run(`INSERT INTO addresses (street, city, province, zip) VALUES (
+  db.run(`INSERT INTO addresses (street, city, province, zip, contact_id) VALUES (
     '${req.body.street}',
     '${req.body.city}',
     '${req.body.province}',
-    '${req.body.zip}'
-  )`)
-
-  res.redirect('/addresses')
+    '${req.body.zip}',
+    ${req.body.contact_id == '' ? null : req.body.contact_id}
+  )`, (err, rows) => {
+    if (err) throw err
+    res.redirect('/addresses')
+  })
 })
 
 router.get('/edit/:id', (req, res) => {
-  db.all(`SELECT * FROM addresses WHERE id=${req.params.id}`, (err, rows) => {
-    if (err) throw err
-    res.render('addresses-edit', {data: rows})
+  db.all(`
+    SELECT
+      addresses.*,
+      contacts.name as contact_name
+
+      FROM addresses
+        LEFT JOIN contacts 
+          ON contacts.id = addresses.contact_id
+            WHERE addresses.id = ${req.params.id}
+  `, (errA, rowsA) => {
+    if (errA) throw errA
+    db.all(`SELECT id, name FROM contacts`, (errC, rowsC) => {
+      res.render('addresses-edit', {
+        data: rowsA,
+        contacts: rowsC
+      })
+    })
   })
 })
 
@@ -33,10 +62,9 @@ router.post('/edit/:id', (req, res) => {
     street = '${req.body.street}',
     city = '${req.body.city}',
     province = '${req.body.province}',
-    zip = '${req.body.zip}'
+    zip = '${req.body.zip}',
+    contact_id = ${req.body.contact_id == '' ? null : req.body.contact_id}
   WHERE id = ${req.params.id}`)
-
-  //
 
   res.redirect('/addresses')
 })
--- routes/profiles.js
+++ routes/profiles.js
@@ -4,35 +4,82 @@ const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('./db/data.db')
 
 router.get('/', (req, res) => {
-  db.all(`SELECT * FROM profiles`, (err, rows) => {
-    if (err) throw err
-    res.render('profiles', {data: rows})
+  db.all(`
+    SELECT
+      profiles.*,
+      contacts.name as contact_name
+      
+      FROM profiles 
+        LEFT JOIN contacts 
+          ON contacts.id=profiles.contact_id
+  `, (errP, rowsP) => {
+    if (errP) throw errP
+      db.all(`
+        SELECT 
+          contacts.id,
+          contacts.name
+
+          FROM contacts 
+            LEFT JOIN profiles 
+              ON contacts.id=profiles.contact_id 
+                WHERE profiles.contact_id is null
+      `, (errC, rowsC) => {
+        if (errC) throw errC
+        res.render('profiles', {
+          data: rowsP,
+          contacts: rowsC
+        })
+      })
   })
 })
 
 router.post('/', (req, res) => {
-  db.run(`INSERT INTO profiles (username, password) VALUES (
+  db.run(`INSERT INTO profiles (username, password, contact_id) VALUES (
     '${req.body.username}',
-    '${req.body.password}'
-  )`)
-
-  res.redirect('/profiles')
+    '${req.body.password}',
+    ${req.body.contact_id == '' ? null : req.body.contact_id}
+  )`, function(err) {
+    if (err) throw err
+    res.redirect('/profiles')
+  })
 })
 
 router.get('/edit/:id', (req, res) => {
-  db.all(`SELECT * FROM profiles WHERE id=${req.params.id}`, (err, rows) => {
-    if (err) throw err
-    res.render('profiles-edit', {data: rows})
+  db.all(`
+    SELECT
+      profiles.*,
+      contacts.name as contact_name
+      
+      FROM profiles 
+        LEFT JOIN contacts 
+          ON contacts.id = profiles.contact_id
+            WHERE profiles.id = ${req.params.id}
+  `, (errP, rowsP) => {
+    if (errP) throw errP
+      db.all(`
+        SELECT 
+          contacts.id,
+          contacts.name
+
+          FROM contacts 
+            LEFT JOIN profiles 
+              ON contacts.id = profiles.contact_id
+      `, (errC, rowsC) => {
+        if (errC) throw errC
+        res.render('profiles-edit', {
+          data: rowsP,
+          contacts: rowsC
+        })
+      })
   })
 })
 
 router.post('/edit/:id', (req, res) => {
   db.run(`UPDATE profiles SET
     username = '${req.body.username}',
-    password = '${req.body.password}'
+    password = '${req.body.password}',
+    contact_id = ${req.body.contact_id == '' ? null : req.body.contact_id}
   WHERE id = ${req.params.id}`)
-
-  //
 
   res.redirect('/profiles')
 })
